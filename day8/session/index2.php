ef6ddf4c6ce2d9cb75e1bf62254db5215c5b5283
ADMIN <ngominhthien_t62@hus.edu.vn>
2019-06-04 18:47:11 +0700

    08052019

---
+++ day8/session/index2.php
@@ -0,0 +1,16 @@
+<?php
+// bắt buộc phải khởi động session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+
+<h1>Khởi động session</h1>
+</body>
+
